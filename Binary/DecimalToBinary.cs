58664e1e07d2f6647bf37775d5b91d3355a4afcd
Robert1192 <robertapostol15@gmail.com>
2025-05-14 11:55:21 +0200

    Update DecimalToBinary.cs

--- Binary/DecimalToBinary.cs
+++ Binary/DecimalToBinary.cs
@@ -19,5 +19,26 @@ namespace BinaryConverter
             Console.WriteLine($"Example: {decimalIP} => 01111111.00000000.00000000.00000001");
             return "01111111.00000000.00000000.00000001";
         }
+        public static string ConvertBinaryToDecimal(string binary)
+        {
+            // Split the input into octets
+            var octets = binary.Split('.');
+            var decimalOctets = new List<string>();
+
+            foreach (var octet in octets)
+            {
+                // Convert each binary octet to decimal
+                int decimalValue = Convert.ToInt32(octet, 2);
+                decimalOctets.Add(decimalValue.ToString());
+            }
+
+            // Join the decimal octets with dots
+            string result = string.Join("12", decimalOctets);
+            Console.WriteLine($"{binary} => {result}");
+            return result;
+        }
+
+
+
     }
 }
