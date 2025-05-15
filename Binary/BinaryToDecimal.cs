dc77d4a56b189f84c60506c78fc8d0a7e4220bc6
Robert1192 <robertapostol15@gmail.com>
2025-05-15 12:19:01 +0200

    Update BinaryToDecimal.cs

--- Binary/BinaryToDecimal.cs
+++ Binary/BinaryToDecimal.cs
@@ -11,32 +11,16 @@ namespace BinaryConverter
         public static void Run()
         {
             Console.WriteLine("Binary to Decimal Converter");
-            Console.Write("Enter a binary IP address: ");
-            string binaryIP = Console.ReadLine();
-            if (string.IsNullOrEmpty(binaryIP) || !IsValidBinaryIP(binaryIP))
-            {
-                Console.WriteLine("Invalid binary IP address.");
-                return;
-            }
+            string binaryIP = "01111111.00000000.00000000.00000001";
             Console.WriteLine($"Binary IP: {binaryIP}");
             string decimalIP = ConvertBinaryToDecimal(binaryIP);
             Console.WriteLine($"Decimal IP: {decimalIP}");
         }
 
-        public static bool IsValidBinaryIP(string binaryIP)
-        {
-            string[] octets = binaryIP.Split('.');
-            if (octets.Length != 4) return false;
-            foreach (string octet in octets)
-            {
-                if (octet.Length != 8 || !octet.All(c => c == '0' || c == '1'))
-                    return false;
-            }
-            return true;
-        }
-
         public static string ConvertBinaryToDecimal(string binary)
         {
+            //biostrong = 10101010 => 170 - 128 +32 + 8 +2
+            //StringSequence 128 64 32 16 8 4 2 1
             List<int> powerOfTwos = new List<int>() { 128, 64, 32, 16, 8, 4, 2, 1 };
             string[] octets = binary.Split('.');
             List<string> decimalOctets = new List<string>();
@@ -54,11 +38,7 @@ namespace BinaryConverter
             }
 
             return string.Join(".", decimalOctets);
-        }
-
-        public static void Main()
-        {
-            Run();
+            string
         }
     }
 }
